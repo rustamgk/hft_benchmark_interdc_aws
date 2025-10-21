77bbc195e281b971b5191af37177cbecf0e88560
Rustam Galimyanov <rustam.gk@gmail.com>
2025-10-21 14:54:19 +0200

    [DEVOPS-XXX] :: Terraform module added for EC2

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# EC2 Module - Creates an EC2 instance in a VPC
+
+# Get the latest Ubuntu 22.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]  # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "main" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  source_dest_check      = var.source_dest_check
+  associate_public_ip_address = var.associate_public_ip
+  key_name               = var.key_name
+  iam_instance_profile   = var.iam_instance_profile != "" ? var.iam_instance_profile : null
+  
+  user_data = var.user_data
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.instance_name
+    }
+  )
+}
+
+# Outputs
+output "instance_id" {
+  value       = aws_instance.main.id
+  description = "Instance ID"
+}
+
+output "public_ip" {
+  value       = aws_instance.main.public_ip
+  description = "Public IP address"
+}
+
+output "private_ip" {
+  value       = aws_instance.main.private_ip
+  description = "Private IP address"
+}
+
+output "primary_network_interface_id" {
+  value       = aws_instance.main.primary_network_interface_id
+  description = "Primary ENI (Elastic Network Interface) ID"
+}
---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,57 @@
+# Variables for EC2 module
+
+variable "instance_name" {
+  type        = string
+  description = "Name of the EC2 instance"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+  default     = "t3.small"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID where instance will be launched"
+}
+
+variable "security_group_id" {
+  type        = string
+  description = "Security group ID"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Name of the SSH key pair"
+}
+
+variable "source_dest_check" {
+  type        = bool
+  description = "Whether to enable source destination check"
+  default     = true
+}
+
+variable "associate_public_ip" {
+  type        = bool
+  description = "Whether to associate a public IP"
+  default     = true
+}
+
+variable "user_data" {
+  type        = string
+  description = "User data script to run on instance startup"
+  default     = ""
+}
+
+variable "iam_instance_profile" {
+  type        = string
+  description = "Optional IAM instance profile name to attach"
+  default     = ""
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to the instance"
+  default     = {}
+}
